b33a5e5e70cd12b6f68fabb4289632d46f094b27
unknown <beto_carvvalho@hotmail.com>
2020-02-03 20:08:34 -0300

    new

---
+++ precache-manifest.8a6173b36becec87d49125e7799010d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80530ab19e205edc902ce3e14bbf2411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a4f28d11255a4777b1a",
+    "url": "/static/css/main.6915e3be.chunk.css"
+  },
+  {
+    "revision": "67d359b4e01550ac2b49",
+    "url": "/static/js/2.275e7df4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.275e7df4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a4f28d11255a4777b1a",
+    "url": "/static/js/main.984187e6.chunk.js"
+  },
+  {
+    "revision": "a81ad0fe1635bda8e950",
+    "url": "/static/js/runtime-main.2a977f36.js"
+  }
+]);
